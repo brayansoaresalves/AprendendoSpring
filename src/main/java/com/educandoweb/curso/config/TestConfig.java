801592b24296c929a7fba52b4c3eac2d1c61ab71
brayansoaresalves <brayan.iub10@gmail.com>
2021-05-22 10:15:05 -0300

    Repositorio JPA, injeção de dependencia e enviando dados para o banco de dados

---
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -0,0 +1,36 @@
+package com.educandoweb.curso.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+
+	// popular o banco de dados 
+	@Autowired // resolver a dependencia e associar uma instancia 
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		
+	}
+	
+	
+	
+	
+
+}
